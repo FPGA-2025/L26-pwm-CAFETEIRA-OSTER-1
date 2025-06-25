d46eac9d1df9ac34a23f9ac6c15e8133a452120e
CAFETEIRA-OSTER <yuribernardosie@gmail.com>
2025-06-24 22:02:31 -0300

    teste

--- pwm_control.v
+++ pwm_control.v
@@ -8,10 +8,14 @@ module PWM_Control #(
 );
     localparam integer PWM_PERIOD = CLK_FREQ / PWM_FREQ;
     localparam SECOND = CLK_FREQ / 2;
+    localparam DUTY = 10000; // 50%
 
     // PWM counter
     reg [31:0] counter_frequency = 0;
     reg [7:0] leds_reg = 8'b00000000;
+
+    reg [31:0] counter = 0;
+    reg [31:0] counter_duty = 0;
 
     assign leds = leds_reg;
 
@@ -19,9 +23,21 @@ module PWM_Control #(
     always @(posedge clk or negedge rst_n) begin
         if (!rst_n) begin
             counter_frequency <= 0;
+            counter <= 0;
         end else begin
             if (counter_frequency <= SECOND - 1 ) begin
                 counter_frequency <= counter_frequency + 1;
+                if(counter <= PWM_PERIOD - 1) begin
+                    counter <= counter + 1;
+                    if (counter_duty <= DUTY - 1 ) begin
+                        leds_reg <= 8'b11111111;
+                    end else begin
+                        leds_reg <= 8'b00000000;
+                    end
+                end else begin
+                    counter <= 0;
+                    counter_duty <= 0;
+                end
             end else begin  
                 leds_reg <= ~leds_reg;
                 counter_frequency <= 0;
